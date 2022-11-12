a2ed2b010983c4b8005e82795347461e6b56ed7e
Hedy Chien <paihsiu0910@gmail.com>
2022-11-12 14:06:04 +0800

    Function

--- lesson6.py
+++ lesson6.py
@@ -1,9 +1,11 @@
-def sayHello():
+def sayHello(): #沒有參數的function
     print("Hello!")
 
+def sayHello_withName(name): #有參數的function
+    print(name)
 
 if __name__ == "__main__":
     sayHello()
+    sayHello_withName("hedy") #引數值傳遞至name參數內
 
 
-    
