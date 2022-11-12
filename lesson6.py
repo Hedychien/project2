45bb6da52c0e6b6d1c68fbcaf4fb7b2f2c525b73
Hedy Chien <paihsiu0910@gmail.com>
2022-11-12 14:22:26 +0800

    square

--- lesson6.py
+++ lesson6.py
@@ -4,8 +4,16 @@ def sayHello(): #沒有參數的function
 def sayHello_withName(name): #有參數的function
     print(name)
 
+def square_area(side):    #有一個參數的function，同時有傳出值
+    area = side **2       #此area為function變數
+    return area
+
+
 if __name__ == "__main__":
     sayHello()
-    sayHello_withName("hedy") #引數值傳遞至name參數內
+    sayHello_withName("Hedy")    #引數值傳遞至name參數內
+    side = eval(input("請輸入正方形的邊:"))
+    area = square_area(side)    #此area為文件變數
+    print(f"正方形,一邊為{side}, 面積為{area}")
 
 
